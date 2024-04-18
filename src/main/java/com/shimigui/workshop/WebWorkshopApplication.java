c4fdc4b617c49c1e34c5482c9768a0c7d6e8b05a
Miguel Nascimento <110462979+ShiMigui@users.noreply.github.com>
2024-04-18 20:16:10 -0300

    new Spring Boot Project

---
+++ src/main/java/com/shimigui/workshop/WebWorkshopApplication.java
@@ -0,0 +1,13 @@
+package com.shimigui.workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebWorkshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shimigui/workshop/WebWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shimigui.workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebWorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
