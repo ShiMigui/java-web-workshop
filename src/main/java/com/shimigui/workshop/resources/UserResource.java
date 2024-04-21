f5e8cf1b4f83512f012e4c2821ecbce9977a1c39
Miguel Nascimento <110462979+ShiMigui@users.noreply.github.com>
2024-04-21 02:18:00 -0300

    Getting posts of an user

--- src/main/java/com/shimigui/workshop/resources/UserResource.java
+++ src/main/java/com/shimigui/workshop/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.shimigui.workshop.resources;
 
 import java.net.URI;
+import java.util.List;
 import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.shimigui.workshop.domain.Post;
 import com.shimigui.workshop.domain.User;
 import com.shimigui.workshop.dto.UserDTO;
 import com.shimigui.workshop.services.UserService;
@@ -35,26 +37,31 @@ public class UserResource {
 	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
 		return ResponseEntity.ok(new UserDTO(service.findBy(id)));
 	}
-	
+
 	@PostMapping
 	public ResponseEntity<Void> insert(@RequestBody UserDTO dto) {
 		User obj = service.insert(dto.getReference());
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 	}
-	
+
 	@DeleteMapping(value = "/{id}")
 	public ResponseEntity<Void> delete(@PathVariable String id) {
 		service.deleteBy(id);
 		return ResponseEntity.noContent().build();
 	}
-	
+
 	@PutMapping(value = "/{id}")
-	public ResponseEntity<UserDTO> update(@RequestBody UserDTO dto, @PathVariable String id){
+	public ResponseEntity<UserDTO> update(@RequestBody UserDTO dto, @PathVariable String id) {
 		User ref = dto.getReference();
 		ref.setId(id);
 		dto.setReference(service.update(ref));
-		
+
 		return ResponseEntity.ok(dto);
+	}
+
+	@GetMapping(value = "/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		return ResponseEntity.ok(service.findBy(id).getPosts());
 	}
 }
